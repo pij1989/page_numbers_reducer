4b31a06372ee5d90e1f42f214cf374902a2ca6f1
pij1989 <dimon.pij@mail.ru>
2019-12-22 00:58:50 +0300

    test PageNumberReducer

--- src/test/java/com/pozharsky/dmitri/util/PageNumberReducerTest.java
+++ src/test/java/com/pozharsky/dmitri/util/PageNumberReducerTest.java
@@ -1,6 +1,7 @@
 package com.pozharsky.dmitri.util;
 
 import com.pozharsky.dmitri.configuration.TestAppConfig;
+import com.pozharsky.dmitri.exception.PageNumberFormatException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
